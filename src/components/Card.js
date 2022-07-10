7e2039f03541f289a8e7f75fa8c93a90d859f460
jeremybeziat <jeremybeziat.pro@gmail.com>
2022-07-10 23:57:14 +0200

    📦 NEW: seo alt img card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,13 @@ import React from "react";
 const Card = (props) => {
   return (
     <figure className="card">
-      <img className="card_img" src={props.src} alt={props.alt} />
+      <img className="card_img" src={props.src} alt={props.createur} />
       <figcaption className="card_figcaption">
         <img className="avatar_card" src={props.icone} alt={props.createur} />
-        <p className="createur"><span className="span_createur"></span>{props.createur}</p>
+        <p className="createur">
+          <span className="span_createur"></span>
+          {props.createur}
+        </p>
       </figcaption>
     </figure>
   );
