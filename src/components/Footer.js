a62b8adbbdc7f0d7ca2cc613344ba0ba2c07f28f
jeremybeziat <jeremybeziat.pro@gmail.com>
2022-07-11 20:25:16 +0200

    🐛 FIX: link footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,19 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <Link className="link_footer" to="/">
+        <Link className="link_footer" to="#">
           Politique de confidentialité
         </Link>
-        <Link className="link_footer" to="/">
+        <Link className="link_footer" to="#">
           Mention légales
         </Link>
-        <Link className="link_footer" to="/">
+        <Link className="link_footer" to="#">
           Conditions générales d'utilisation
         </Link>
-        <Link className="link_footer" to="/">
+        <Link className="link_footer" to="#">
           Contact
         </Link>
-        <Link className="link_footer" to="/">
+        <Link className="link_footer" to="#">
           Aide
         </Link>
       </div>
